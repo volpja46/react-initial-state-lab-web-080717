df94cd55281e7f52b1aae47b8bfbe9d6eaf6650c
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-10-10 13:13:30 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Bomb from './components/Bomb';
 ReactDOM.render(
   <div>
     <ImageSlider></ImageSlider>
-    <Bomb initialCount={120} />
+    <Bomb initialCount={0} />
   </div>,
   document.getElementById('root')
 );
